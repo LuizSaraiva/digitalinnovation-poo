2f3e60ce078798f35030fcd58e2a8b2ec0c008a9
luiz henrique <l.henrique.saraiva@gmail.com>
2021-02-25 20:19:08 -0300

    Created account class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta(val agencia:String, val numero:String, val saldo: BigDecimal) {
+
+    fun deposito(valor: BigDecimal){}
+
+    fun saque(valor: BigDecimal){}
+}
