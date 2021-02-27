4854f7503aac0ea7df0b5a8a92cea58f2a1b0615
luiz henrique <l.henrique.saraiva@gmail.com>
2021-02-27 19:37:35 -0300

    Update employee manager test

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,8 +3,9 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val gerente = Gerente("Luiz", "456789",500.0)
+    val gerente = Gerente("Luiz", "456789",500.0,"1234")
 
     ImprimeRelatorioFuncionario.imprime(gerente)
 
+    TesteAutenticacao().autentica(gerente)
 }
